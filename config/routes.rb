b32524ce9a1ddea87f5a8023176e8b3069e46fb8
Eli Spurlock <espurlock@localhost.localdomain>
2018-03-13 13:24:46 -0400

    working point

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  get 'welcome/index'
+ 
+  root 'welcome#index'
+end
+
+Rails.application.routes.draw do
+  get 'welcome/index'
+ 
+  resources :articles
+ 
+  root 'welcome#index'
 end
