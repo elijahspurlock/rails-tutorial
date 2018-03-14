338af448d0eebb9f7ee15fff30ddbf12d042464e
Eli Spurlock <elijahspurlock@mail.weber.edu>
2018-03-14 09:43:15 -0600

    deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,11 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:commenter, :body)
     end
-end
+  end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
