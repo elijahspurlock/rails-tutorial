11c43ca44c1377b07345b318a13bcda67c3a93d0
Eli Spurlock <elijahspurlock@mail.weber.edu>
2018-03-14 15:50:28 -0600

    created user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'welcome/index'
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'welcome/index'
  
   root 'welcome#index'
 end
 
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'welcome/index'
  
    	resources :articles do
