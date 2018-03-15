e7ef6d2f3373442c0cf1ce61d834a7577c3b40e6
Eli Spurlock <elijahspurlock@mail.weber.edu>
2018-03-15 12:14:43 -0600

    created proper user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -9,4 +9,27 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+=begin  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+=end
+=begin  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+=end
 end
+
+
